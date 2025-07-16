107cbe8998d8ed88e75e551238f711647633b1d1
Bob Liotta <bob@liotta.us>
2025-07-16 17:03:41 -0400

    changes

--- assets/js/logo-switcher.js
+++ assets/js/logo-switcher.js
@@ -3,10 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const logo = document.getElementById("dynamic-logo");
   if (!logo) return;
 
-  if (ref.includes("google.com")) {
+  if (ref.includes("liotta.us")) {
     logo.src = "{{ '/assets/images/logo-google.png' | relative_url }}";
   } else if (ref.includes("facebook.com")) {
     logo.src = "{{ '/assets/images/logo-facebook.png' | relative_url }}";
+  } else {
+    logo.src = "{{ '/assets/images/logo.png' | relative_url }}";
   }
 });
 
